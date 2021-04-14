8460a938030c558a9bea7b18c09587ae56c6b3e7
Toby Zerner <toby.zerner@gmail.com>
2021-04-14 14:46:30 +0930

    menu: remove keydown event listener on disconnect

--- src/menu/menu.ts
+++ src/menu/menu.ts
@@ -12,39 +12,45 @@ class MenuElement extends HTMLElement {
             el.setAttribute('tabindex', '-1');
         });
 
-        document.addEventListener('keydown', e => {
-            if (this.hidden) return;
-            if (e.key === 'ArrowUp') {
-                this.navigate(-1);
-                e.preventDefault();
-            } else if (e.key === 'ArrowDown') {
-                this.navigate(1);
-                e.preventDefault();
-            } else {
-                // Only respond to letter keys
-                if (e.key.length > 1) return;
+        document.addEventListener('keydown', this.handleKeydown);
+    }
 
-                // If the letter key is part of a key combo,
-                // let it do whatever it was going to do
-                if (e.ctrlKey || e.metaKey || e.altKey) return;
+    public disconnectedCallback(): void {
+        document.removeEventListener('keydown', this.handleKeydown);
+    }
 
-                e.preventDefault();
+    private handleKeydown = (e): void => {
+        if (this.hidden) return;
+        if (e.key === 'ArrowUp') {
+            this.navigate(-1);
+            e.preventDefault();
+        } else if (e.key === 'ArrowDown') {
+            this.navigate(1);
+            e.preventDefault();
+        } else {
+            // Only respond to letter keys
+            if (e.key.length > 1) return;
 
-                this.search += e.key.toLowerCase();
+            // If the letter key is part of a key combo,
+            // let it do whatever it was going to do
+            if (e.ctrlKey || e.metaKey || e.altKey) return;
 
-                clearTimeout(this.searchTimeout);
-                this.searchTimeout = window.setTimeout(() => {
-                    this.search = '';
-                }, MenuElement.searchDelay);
+            e.preventDefault();
 
-                this.focusableItems.some(el => {
-                    if (el.textContent?.trim().toLowerCase().indexOf(this.search) === 0) {
-                        el.focus();
-                        return true;
-                    }
-                });
-            }
-        });
+            this.search += e.key.toLowerCase();
+
+            clearTimeout(this.searchTimeout);
+            this.searchTimeout = window.setTimeout(() => {
+                this.search = '';
+            }, MenuElement.searchDelay);
+
+            this.focusableItems.some(el => {
+                if (el.textContent?.trim().toLowerCase().indexOf(this.search) === 0) {
+                    el.focus();
+                    return true;
+                }
+            });
+        }
     }
 
     private navigate(step: number) {
